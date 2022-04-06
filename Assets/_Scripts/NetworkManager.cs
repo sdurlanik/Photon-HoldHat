49149dd3db67d5a04a2335389d61dfe62410a132
sdurlanik <sametdurlanik@gmail.com>
2022-04-06 22:15:56 +0300

    Setup photon network and menu ui

---
+++ Assets/_Scripts/NetworkManager.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+   public static NetworkManager instance;
+
+   private void Awake()
+   {
+      if (instance != null && instance != this)
+      {
+         gameObject.SetActive(false);
+      }
+      else
+      {
+         instance = this;
+         DontDestroyOnLoad(gameObject);
+
+      }
+   }
+   
+   private void Start()
+   {
+      PhotonNetwork.ConnectUsingSettings();
+   }
+
+ 
+   public void CreateRoom(string roomName)
+   {
+      PhotonNetwork.CreateRoom(roomName);
+   }
+
+   public void JoinRoom(string roomName)
+   {
+      PhotonNetwork.JoinRoom(roomName);
+   }
+
+   [PunRPC]
+   public void ChangeScene(string sceneName)
+   {
+      PhotonNetwork.LoadLevel(sceneName);
+   }
+}

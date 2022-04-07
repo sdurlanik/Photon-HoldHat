c684a3752252fc748c284ff958699e358489f511
sdurlanik <sametdurlanik@gmail.com>
2022-04-07 03:19:07 +0300

    Created hat giving

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    [Header("Stats")] 
+    [SerializeField] private bool _gameEnded;
+    [SerializeField] private float _timeToWin;
+    [SerializeField] private float _invincibleDuration;
+    private float _hatPickupTime;
+
+    [Header("Players")] 
+    [SerializeField] private string _playerPrefabLocation;
+    [SerializeField] private Transform[] _spawnPoints;
+    private int _playersInGame;
+    public PlayerController[] players;
+    public int playerWithHat;
+
+
+
+    public static GameManager instance;
+    
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        players = new PlayerController[PhotonNetwork.PlayerList.Length];
+        photonView.RPC("ImInGame", RpcTarget.AllBuffered);
+    }
+
+    [PunRPC]
+    void ImInGame()
+    {
+        _playersInGame++;
+        if (_playersInGame == PhotonNetwork.PlayerList.Length)
+        {
+            SpawnPlayer();    
+        }
+    }
+
+    void SpawnPlayer()
+    {
+        GameObject playerObj = PhotonNetwork.Instantiate(_playerPrefabLocation,
+            _spawnPoints[Random.Range(0, _spawnPoints.Length)].position, Quaternion.identity);
+        
+        PlayerController playerScript = playerObj.GetComponent<PlayerController>();
+        
+        playerScript.photonView.RPC("Initialize", RpcTarget.All, PhotonNetwork.LocalPlayer);
+    }
+
+    public PlayerController GetPlayer(int playerId)
+    {
+        return players.First(x => x.id == playerId);
+    }
+
+    public PlayerController GetPlayer(GameObject playerObj)
+    {
+        return players.First(x => x.gameObject == playerObj);
+    }
+
+    [PunRPC]
+    public void GiveHat(int playerId, bool initialGive)
+    {
+        if (!initialGive)
+            GetPlayer(playerWithHat).SetHat(false);
+        
+        playerWithHat = playerId;
+        GetPlayer(playerId).SetHat(true);
+        _hatPickupTime = Time.time;
+    }
+
+    public bool CanGetHat()
+    {
+        if (Time.time > _hatPickupTime + _invincibleDuration)
+            return true;
+        else
+            return false;
+       
+    }
+}
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,15 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviourPunCallbacks
 {
      [HideInInspector] public int id;
 
      [Header("Info")] 
      [SerializeField] private float _moveSpeed;
-     [SerializeField] private float _jumpSpeed;
+     [SerializeField] private float _jumpForce;
      [SerializeField] private GameObject hatObject;
 
      [HideInInspector] public float curHatTime;
@@ -18,4 +20,70 @@ public class PlayerController : MonoBehaviour
      [SerializeField] private Rigidbody _rigidbody;
      [SerializeField] private Player _photonPlayer;
 
+     [PunRPC]
+     public void Initialize(Player player)
+     {
+          _photonPlayer = player;
+          id = player.ActorNumber;
+
+          GameManager.instance.players[id - 1] = this;
+
+          if (id == 1)
+          {
+               GameManager.instance.GiveHat(id,true);
+          }
+
+          if (!photonView.IsMine)
+          {
+               _rigidbody.isKinematic = true;
+          }
+     }
+     private void Update()
+     {
+          Move();
+
+          if (Input.GetKeyDown(KeyCode.Space))
+          {
+               TryJump();
+          }
+     }
+
+     void Move()
+     {
+          float x = Input.GetAxis("Horizontal") * _moveSpeed;
+          float z = Input.GetAxis("Vertical") * _moveSpeed;
+
+          _rigidbody.velocity = new Vector3(x, _rigidbody.velocity.y, z);
+     }
+
+     void TryJump()
+     {
+          Ray ray = new Ray(transform.position, Vector3.down);
+
+          if (Physics.Raycast(ray,0.7f))
+          {
+               _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);     
+          }
+     }
+
+     public void SetHat(bool hasHat)
+     {
+          hatObject.SetActive(hasHat);
+     }
+
+     void OnCollisionEnter(Collision collision)
+     {
+          if (!photonView.IsMine)
+               return;
+          if (collision.gameObject.CompareTag("Player"))
+          {
+               if (GameManager.instance.GetPlayer(collision.gameObject).id == GameManager.instance.playerWithHat)
+               {
+                    if (GameManager.instance.CanGetHat())
+                    {
+                         GameManager.instance.photonView.RPC("GiveHat", RpcTarget.All,id,false);
+                    }
+               }
+          }
+     }
 }
